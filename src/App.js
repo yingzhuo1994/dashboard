d4f005875f9b9b515925160b5b8cb9f6b5aeff8c
yingzhuo <yingzhuo1994@gmail.com>
2023-01-06 21:36:25 -0600

    add menu bar part

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 
 const App = () => {
-
+  const activeMenu = false;
   return (
     <div>
       <BrowserRouter>
@@ -27,6 +27,18 @@ const App = () => {
 
             </TooltipComponent>
           </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">Sidebar</div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+          )}
+          <div className={`dark:bg-main-dark-bg  bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72': 'flex-2'}`}>
+            <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+              Navbar
+            </div>
+          </div>
+
+          
 
         </div>
       </BrowserRouter>
