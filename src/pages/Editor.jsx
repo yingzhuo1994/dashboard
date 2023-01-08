876fe96f95fb2e23c639d4c94b2e3866eaf0c1af
yingzhuo <yingzhuo1994@gmail.com>
2023-01-07 21:18:37 -0600

    add more contents for toolbar

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -3,11 +3,20 @@ import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent,
 
 import { Header } from '../components';
 import { EditorData } from '../data/dummy';
+const toolbarSettings = {
+  items: ['Bold', 'Italic', 'Underline', 'StrikeThrough',
+      'FontName', 'FontSize', 'FontColor', 'BackgroundColor',
+      'LowerCase', 'UpperCase', '|',
+      'Formats', 'Alignments', 'OrderedList', 'UnorderedList',
+      'Outdent', 'Indent', '|',
+      'CreateLink', 'Image', '|', 'ClearFormat', 'Print',
+      'SourceCode', 'FullScreen', '|', 'Undo', 'Redo']
+};
 
 const Editor = () => (
   <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
     <Header category="App" title="Editor" />
-    <RichTextEditorComponent>
+    <RichTextEditorComponent toolbarSettings={toolbarSettings} >
       <EditorData />
       <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
     </RichTextEditorComponent>
